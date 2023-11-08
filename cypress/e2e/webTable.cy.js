065e1d578523116035d46511f1abf7b9565f5a52
rwayf001 <fezisa.rwayi@vcontractor.co.za>
2023-11-08 21:50:35 +0200

    Additional checks on UI code and API tests added

--- cypress/e2e/webTable.cy.js
+++ cypress/e2e/webTable.cy.js
@@ -1,16 +1,15 @@
-describe('template spec', () => {
+describe('WebTable', () => {
   beforeEach(() => {
     cy.visit('https://www.way2automation.com/angularjs-protractor/webtables/')
 
     // Assert on page title
     cy.title().should('eq','Protractor practice website - WebTables')
 
-    // Check that the page title is visible
-  //  cy.title().should('be.visible')
-
     // Assert on table headings
     cy.get('.smart-table-header-row').should('contain','First Name').should('contain','Last Name').should('contain','User Name').should('contain','Customer').should('contain','Role').should('contain','E-mail').should('contain','Cell Phone').should('contain','Locked')
      
+    // Check 'Add User' button label is correct
+    cy.get('.btn.btn-link.pull-right').should('have.text', ' Add User')
 
   })
 
@@ -18,9 +17,9 @@ describe('template spec', () => {
 
 
     //Add user 1
-    // Click on 'Add' button
+    // Click on 'Add User' button
     cy.get('.smart-table-global-search-row [ng-show="actions.add.url"] [type="add"] [ng-class="iconClass"]').click()
-    cy.wait(2000)
+    cy.wait(1000)
 
     // Add user details on 'First Name' field
     cy.get('.smart-table-edit-data-cell .ng-scope [name="FirstName"]').type('FName1')
@@ -30,7 +29,7 @@ describe('template spec', () => {
 
     // Add user details on 'User Name' field
     var userNameNum = (userNameNum, Math.floor(Math.random() * 1000))
-    var userName = "user" + userNameNum
+    var userName = "User" + userNameNum
 
     cy.get('.smart-table-edit-data-cell .ng-scope [name="UserName"]').type(userName)
 
@@ -49,16 +48,29 @@ describe('template spec', () => {
     // Add user details on 'Cell' field
     cy.get('.smart-table-edit-data-cell .ng-scope [name="Mobilephone"]').type('082555')    
 
+    // Check 'Save' button label correct
+    cy.get('.btn.btn-success').should('have.text', 'Save')
+
     // Click on 'Save' button
     cy.get('.modal-footer [class="btn btn-success"]').click()
     cy.wait(2000)
 
+    //Assert that all user details have been added to the list
+    cy.get('.smart-table-data-row:first-child td:first-child').contains('FName1')
+    cy.get('.smart-table-data-row:first-child td:nth-child(2)').contains('LName1')
+    cy.get('.smart-table-data-row:first-child td:nth-child(3)').contains(userName)
+    //I removed the below code due to failure of the Customer field being populated    
+    //cy.get('.smart-table-data-row:first-child td:nth-child(5)').contains('Company AAA')
+    cy.get('.smart-table-data-row:first-child td:nth-child(6)').contains('Admin')
+    cy.get('.smart-table-data-row:first-child td:nth-child(7)').contains('admin@mail.com')
+    cy.get('.smart-table-data-row:first-child td:nth-child(8)').contains('082555')
+    
 
-
-    //Add user 2
-    // Click on 'Add' button
+    
+    //Add User 2
+    // Click on 'Add User' button
     cy.get('.smart-table-global-search-row [ng-show="actions.add.url"] [type="add"] [ng-class="iconClass"]').click()
-    cy.wait(2000)
+    cy.wait(1000)
 
     // Add user details on 'First Name' field
     cy.get('.smart-table-edit-data-cell .ng-scope [name="FirstName"]').type('{selectAll}{backspace}')
@@ -70,17 +82,17 @@ describe('template spec', () => {
 
     // Add user details on 'User Name' field
     var userNameNum2 = (userNameNum2, Math.floor(Math.random() * 1000))
-    var userName2 = "user" + userNameNum2
+    var userName2 = "User" + userNameNum2
 
     cy.get('.smart-table-edit-data-cell .ng-scope [name="UserName"]').type('{selectAll}{backspace}')
     cy.get('.smart-table-edit-data-cell .ng-scope [name="UserName"]').type(userName2)
 
     // Add user details on 'Password' field
     cy.get('.smart-table-edit-data-cell .ng-scope [name="Password"]').type('{selectAll}{backspace}')    
-    cy.get('.smart-table-edit-data-cell .ng-scope [name="Password"]').type('Pass2') 
+    cy.get('.smart-table-edit-data-cell .ng-scope [name="Password"]').type('Pass2')    
 
     // Add user details on 'Customer' field
-    cy.get('.smart-table-edit-data-cell .ng-scope .radio [value="16"]').click()
+    cy.get('.smart-table-edit-data-cell .ng-scope .radio [value="15"]').click()
 
     // Add user details on 'Role' field
     cy.get('.smart-table-edit-data-cell .ng-scope [name="RoleId"]').select('Customer')
@@ -90,12 +102,25 @@ describe('template spec', () => {
     cy.get('.smart-table-edit-data-cell .ng-scope [name="Email"]').type('cusomter@mail.com')
 
     // Add user details on 'Cell' field
-    cy.get('.smart-table-edit-data-cell .ng-scope [name="Mobilephone"]').type('{selectAll}{backspace}')    
-    cy.get('.smart-table-edit-data-cell .ng-scope [name="Mobilephone"]').type('083444') 
+    cy.get('.smart-table-edit-data-cell .ng-scope [name="Mobilephone"]').type('{selectAll}{backspace}') 
+    cy.get('.smart-table-edit-data-cell .ng-scope [name="Mobilephone"]').type('083444')    
+
+    // Check 'Save' button label correct
+    cy.get('.btn.btn-success').should('have.text', 'Save')
 
     // Click on 'Save' button
     cy.get('.modal-footer [class="btn btn-success"]').click()
-    
+    cy.wait(2000)
+
+    //Assert that all user details have been added to the list
+    cy.get('.smart-table-data-row:first-child td:first-child').contains('FName2')
+    cy.get('.smart-table-data-row:first-child td:nth-child(2)').contains('LName2')
+    cy.get('.smart-table-data-row:first-child td:nth-child(3)').contains(userName2)
+    //I removed the below code due to failure of the Customer field being populated    
+    //cy.get('.smart-table-data-row:first-child td:nth-child(5)').contains('Company BBB')
+    cy.get('.smart-table-data-row:first-child td:nth-child(6)').contains('Customer')
+    cy.get('.smart-table-data-row:first-child td:nth-child(7)').contains('cusomter@mail.com')
+    cy.get('.smart-table-data-row:first-child td:nth-child(8)').contains('083444')
 
   })
 
